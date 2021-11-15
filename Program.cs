93687451cfeca3c0f745b0663dcb419774581d81
trexa81 <trex81@yandex.ru>
2021-11-15 18:40:06 +0300

    new lesson6

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // new lesson6
+         
+        }
+    }
+}
+//6.1
