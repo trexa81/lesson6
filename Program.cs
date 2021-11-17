cb28002d607422a3ab85cbfded88fbe02d14b66b
trexa81 <trex81@yandex.ru>
2021-11-17 18:08:19 +0300

    прдолжим

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace lesson6// new brench
 {
@@ -6,7 +7,17 @@ namespace lesson6// new brench
     {
         static void Main(string[] args)
         {
-         
+            Process.Start("notepad.exe");
+            var processes = Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                Console.WriteLine($"{nameof(process.ProcessName)}: {process.ProcessName}, {nameof(Process.Id)}: {process.Id}");
+            }
+            Console.WriteLine("Введите Id процесса для его завершения");
+            var concoleReadLine = Console.ReadLine();
+            var killName = Process.GetProcessById(Convert.ToInt32(concoleReadLine));
+            killName.Kill();
+            //Console.ReadKey();
         }
     }
 }
