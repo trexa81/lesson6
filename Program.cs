5823c086b61b409f421fa8a8494ceaa86d711ec8
trexa81 <trex81@yandex.ru>
2021-11-18 20:37:22 +0300

    killName.Kill

--- Program.cs
+++ Program.cs
@@ -13,10 +13,44 @@ namespace lesson6// new brench
             {
                 Console.WriteLine($"{nameof(process.ProcessName)}: {process.ProcessName}, {nameof(Process.Id)}: {process.Id}");
             }
-            Console.WriteLine("Введите Id процесса для его завершения");
-            var concoleReadLine = Console.ReadLine();
-            var killName = Process.GetProcessById(Convert.ToInt32(concoleReadLine));
-            killName.Kill();
+            Console.WriteLine("Введите Id процесса или его имя для его завершения");
+
+            int number;
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+            
+            bool result = int.TryParse(input, out number);
+            if (result == true)
+            //Console.WriteLine($"Преобразование прошло успешно" + number) ;
+            {
+                var killID = Process.GetProcessById(number);
+                Console.WriteLine($"Для завершения процесса: " + number + " нажмите любую кнопку.");
+                Console.ReadKey();
+                killID.Kill();
+            }
+            else
+            {
+
+                foreach (var killName in Process.GetProcessesByName(input))
+                {
+                    Console.WriteLine($"Для завершения процесса: " + number + " нажмите любую кнопку.");
+                    Console.ReadKey();
+                    killName.Kill();
+                }
+                Console.WriteLine($"Преобразование завершилось неудачно" + input);
+            }
+
+            //var concoleReadLine = Console.ReadLine();
+            //Console.WriteLine(killID);
+            //Console.WriteLine("Введите имя процесса для его завершения");
+            //var concoleReadLine2 = Console.ReadLine();
+            //foreach (var killName in Process.GetProcessesByName(concoleReadLine2))
+            //{
+            //    Console.WriteLine(killName);
+            //    Console.ReadKey();
+            //    killName.Kill();
+            //}
+            //killName.Kill();
             //Console.ReadKey();
         }
     }
